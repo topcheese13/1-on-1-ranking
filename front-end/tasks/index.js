a3306d8fdcc12b9ac0e1670d00f9fc59245c751f
Stéphane LaFlèche <stephane.lafleche@gmail.com>
2018-03-30 20:39:23 -0400

    Updating to newer gulp plugins

--- front-end/tasks/index.js
+++ front-end/tasks/index.js
@@ -1,5 +1,6 @@
 import path    from 'path'
-const gulp = require('gulp');
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
 const autoprefixer = require('gulp-autoprefixer');
 import { scripts } from './webpack'
 import { server }  from './server'
@@ -7,46 +8,88 @@ const sass = require('gulp-sass');
 const clean = require('gulp-clean');
 const sourcemaps = require('gulp-sourcemaps');
 const isProduction = (process.env.NODE_ENV === 'production');
+import {exec} from 'child_process';
+import browserSync from 'browser-sync';
+import webpack from 'webpack';
+import webpackStream from 'webpack-stream';
 
-// Clean
+const plugins = gulpLoadPlugins();
+const reload = browserSync.reload;
 
-gulp.task('clean-scripts', function () {
-    return gulp.src('../../public/js', {read: false})
-        .pipe(clean());
-});
+const workingPath = './front-end/';
+const destPath = './public/';
 
-gulp.task('clean-styles', function () {
-    return gulp.src('../../public/css', {read: false})
-        .pipe(clean());
-});
+/* The Sass data is concatenated and minified then outputed. */
+export const scss = () => gulp
+    .src(`${workingPath}/scss/**/*.scss`)
+    .pipe(plugins.sass())
+    .pipe(gulp.dest(`${destPath}/css`))
+    .pipe(browserSync.stream());
 
-gulp.task('clean', gulp.series('clean-scripts', 'clean-styles'));
+// Clean JS
+export const clean_scripts = () => gulp
+    .src(`${destPath}/js`, {read: false})
+    .pipe(clean());
 
-// Watch
-gulp.task('watch', gulp.watch('scss/**/*.scss',
-    gulp.parallel('styles'))
-);
+// Clean CSS
+export const clean_css = () => gulp
+    .src(`${destPath}/css`, {read: false})
+    .pipe(clean());
 
 
-// SCSS
-// gulp.task('styles', function () {
-//     return gulp.src('../scss/*.scss')
-//         .pipe($.plumber())
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe($.sass({
-//             errLogToConsole: true
-//         }))
-//         .pipe($.autoprefixer({
-//             "browsers": ["ie > 10", "last 6 iOS versions", "last 4 versions"]
-//         }))
-//         .pipe(sourcemaps.write('./maps'))
-//         .pipe(gulp.dest('../../public/css/'))
-//         .pipe($.size({showFiles: true}));
-// });
+// Clean All
+export const clean_all = () => gulp
+    .parallel(clean_scripts, clean_css);
 
-// JS
-export const dev   = gulp.series('clean', 'styles', server);
-export const build = gulp.series('clean', 'styles', scripts );
+
+// // Scripts
+// export const scripts = () => gulp
+//     .src([
+//         `${workingPath}/js/**/*.js`,
+//         `${workingPath}/js/**/*.jsx`
+//     ])
+//     .pipe(webpackStream(webpackConfig, webpack))
+//     .pipe(plugins.concat('scripts.min.js'))
+//     .pipe(gulp.dest(`${destPath}/js`))
+//     .pipe(browserSync.stream());
+
+
+// Watcher
+export const watch = done => {
+    /* Trigger a SCSS injection (no reload) on a scss file change */
+    gulp.series(scss);
+    gulp.watch(`${workingPath}/scss/**/*.scss`, gulp.parallel(scss));
+
+    /* Trigger a reaload on a js file change */
+    let scriptsWatcher = gulp.watch(`${workingPath}/js/**/*.js`, gulp.parallel(scripts));
+    scriptsWatcher.on('change', (event, path, stats) => {
+        reload();
+    });
+    done();
+};
+
+/* Main development server command
+ * Runs other gulp tasks to minify CSS and JS files.
+ * Runs the Watch task.
+ * BrowserSync loads hot-reload. */
+gulp.task('dev_tasks', gulp.series(
+    clean_all,
+    gulp.parallel(scss, server, watch),
+    () => {
+        browserSync.init(null, {
+            proxy: "localhost:5000",
+            port: 8080,
+            reloadDelay: 1000,
+            reloadDebounce: 2000,
+            reloadOnRestart: true,
+            open: false
+        });
+    }
+));
+
+
+export const dev   = gulp.series('dev_tasks', scss, server);
+// export const build = gulp.series(clean_all, scss, scripts);
 
 
 
