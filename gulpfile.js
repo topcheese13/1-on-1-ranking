74ba023d6b00546eeadfa89c6590f41155b57008
Stéphane LaFlèche <stephane.lafleche@gmail.com>
2018-04-02 22:57:38 -0400

    Working on webpack config

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,13 @@
 import path from 'path'
+import { compileScripts, runHot } from 'webpack';
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import {exec} from 'child_process';
-import browserSync from 'browser-sync';
 const clean = require('gulp-clean');
-import webpack from 'webpack';
-import webpackStream from 'webpack-stream';
-import { webpackConfig, server } from './front-end/tasks/webpack.js'
+const livereload = require("gulp-livereload");
 
 const isProduction = (process.env.NODE_ENV === 'production');
-
 const plugins = gulpLoadPlugins();
-
-const loc = 'node_modules/';
-const reload = browserSync.reload;
-
 const workingPath = path.resolve(__dirname, 'front-end');
 const destPath = path.resolve(__dirname, 'public');
 
@@ -28,6 +21,7 @@ gulp.task('clean', (done) => {
             `${destPath}/js`,
             `${destPath}/css`,
         ], { allowEmpty: true })
+        .pipe(clean())
         .pipe(plugins.clean());
     done();
 });
@@ -37,14 +31,7 @@ gulp.task('scss', (done) => {
     gulp
         .src([`${workingPath}/scss/**/*.scss`], { allowEmpty: true })
         .pipe(plugins.sass())
-        .pipe(gulp.dest(`${destPath}/css`))
-        .pipe(browserSync.stream());
-    done();
-});
-
-gulp.task('watch', (done) => {
-    console.log(">> Task: Watch");
-    gulp.watch(`${workingPath}/scss/**/*.scss`, gulp.series('scss'));
+        .pipe(gulp.dest(`${destPath}/css`));
     done();
 });
 
@@ -56,14 +43,23 @@ gulp.task('dev', (done) => {
 
 gulp.task('scripts', (done) => {
     console.log(">> Task: Scripts");
-    gulp
-        .src([
+    if (isProduction) {
+        gulp.series(compileScripts);
+    } else {
+        gulp.parallel(runHot, compileScripts);
+    }
+    done();
+});
+
+gulp.task('watch', (done) => {
+    console.log(">> Task: Watch");
+    gulp.watch(`${workingPath}/scss/**/*.scss`, gulp.series('scss'));
+    gulp.watch([
             `${workingPath}/js/**/*.js`,
-            `${workingPath}/components/**/*.jsx`
-        ], { allowEmpty: true })
-        .pipe(webpackStream(webpackConfig, webpack))
-        .pipe(gulp.dest(`${destPath}`))
-        .pipe(browserSync.stream());
+            `${workingPath}/js/**/*.jsx`
+        ],
+        gulp.series('scripts')
+    );
     done();
 });
 
@@ -71,16 +67,7 @@ gulp.task('dev', gulp.series(
     'clean',
     'scss',
     'scripts',
-    server,
-    'watch',
-    () => {
-        // browserSync.init(null, {
-        //     proxy: "localhost:5000",
-        //     port: 8080,
-        //     reloadDelay: 1000,
-        //     reloadDebounce: 2000,
-        //     reloadOnRestart: true,
-        //     open: false
-        // });
-    }
+    'watch'
 ));
+
+export default gulp.task('dev');
