d873356558f8d6aff3973d20f8d739d8c1f9676f
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-05-10 19:48:28 -0400

    Improved gulp file to use autoprefixer and minify

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import {exec} from 'child_process';
 const clean = require('gulp-clean');
-const livereload = require("gulp-livereload");
-
 const isProduction = (process.env.NODE_ENV === 'production');
 const plugins = gulpLoadPlugins();
 const workingPath = path.resolve(__dirname, 'front-end');
 const destPath = path.resolve(__dirname, 'public');
+const autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-minify');
 
 console.log("working path: ", workingPath);
 console.log("destPath path: ", destPath);
@@ -19,7 +19,7 @@ gulp.task('clean', (done) => {
     gulp
         .src([
             `${destPath}/css`,
-        ], { allowEmpty: true })
+        ], { allowEmpty: false })
         .pipe(clean())
         .pipe(plugins.clean());
     done();
@@ -30,6 +30,11 @@ gulp.task('scss', (done) => {
     gulp
         .src([`${workingPath}/scss/**/*.scss`], { allowEmpty: true })
         .pipe(plugins.sass())
+        .pipe(autoprefixer({
+            browsers: ["ie > 9", "last 6 iOS versions", "last 4 versions"],
+            cascade: false
+        }))
+        .pipe(minify({}))
         .pipe(gulp.dest(`${destPath}/css`));
     done();
 });
